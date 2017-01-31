797f1ff8e8161ba8e3dbf4116702919891cbdd1b
Brent Vatne <brentvatne@gmail.com>
2017-01-30 18:03:18 -0800

    Update for sdk-13 and add font loading for Roboto and Roboto_medium

--- main.js
+++ main.js
@@ -1,31 +1,53 @@
 import Exponent from 'exponent';
 import React from 'react';
 import {
+  Platform,
   StatusBar,
   StyleSheet,
   View,
 } from 'react-native';
 import {
-  Content,
+  Button,
   Icon,
 } from 'native-base';
 
 class IconExample extends React.Component {
   render() {
     return (
-      <Content style={{paddingTop: 50}}>
+      <View style={{paddingTop: 50}}>
         <Icon name='ios-home' />
         <Icon name='ios-menu' style={{fontSize: 20, color: 'red'}} />
-      </Content>
+      </View>
     );
   }
 }
 
 class App extends React.Component {
+  state = {
+    isReady: false,
+  }
+
+  async componentWillMount() {
+    if (Platform.OS === 'android') {
+      await Exponent.Font.loadAsync({
+        'Roboto': require('native-base/Fonts/Roboto.ttf'),
+        'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+      });
+    }
+
+    this.setState({isReady: true});
+  }
   render() {
+    if (!this.state.isReady) {
+      return <Exponent.Components.AppLoading />;
+    }
+
     return (
       <View style={styles.container}>
         <IconExample />
+        <Button style={{alignSelf: 'center'}}>
+          This is indeed a button
+        </Button>
         <StatusBar barStyle="default" />
       </View>
     );
